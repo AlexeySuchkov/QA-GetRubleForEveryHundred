4d384ffd9a608389ad28b5ce8cb10024038ed0b8
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-03-01 18:44:34 +0300

    Corrected

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        long depositedAmount = 1100;
+        long initialAccount = 100;
+        long limit = 1000;
+        long bonus;
+        if (depositedAmount >= limit) {
+            bonus = depositedAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        long currentAccount = depositedAmount + bonus + initialAccount;
+        System.out.println(currentAccount);
+    }
 }
